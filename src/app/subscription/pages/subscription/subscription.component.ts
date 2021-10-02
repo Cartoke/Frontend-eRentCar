064bd411574e649f52544cdfa8da75ee91324fa5
SebastianBran <sebastianbranzapata@gmail.com>
2021-10-02 02:14:18 -0500

    Subscription section
    the subscription section was added, which will allow the customer to choose a service plan

--- src/app/subscription/pages/subscription/subscription.component.ts
+++ src/app/subscription/pages/subscription/subscription.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SubscriptionService} from "../../services/subscription.service";
+import {Plan} from "../../model/plan";
 
 @Component({
   selector: 'app-subscription',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./subscription.component.css']
 })
 export class SubscriptionComponent implements OnInit {
+  plans!: Plan[];
 
-  constructor() { }
+  constructor(private subscriptionService: SubscriptionService) { }
 
   ngOnInit(): void {
+    this.getPlans();
   }
+
+  getPlans() {
+    this.subscriptionService.getPlans().subscribe((response: any) => {
+      this.plans = response;
+    });
+  }
+
 
 }
--- src/app/subscription/services/subscription.service.ts
+++ src/app/subscription/services/subscription.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
 import {catchError, retry} from "rxjs/operators";
-import {MySubscription} from "../model/my-subscription";
+import {Plan} from "../model/plan";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionService {
-  basePath = "http://localhost:3000/api/v1/Subscriptions"
+  basePath = "http://localhost:3000/api/v1/plans"
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -35,41 +35,8 @@ export class SubscriptionService {
     return throwError('Something happened with request, please try again later');
   }
 
-  // Create Student
-  create(item: any): Observable<MySubscription> {
-    return this.http.post<MySubscription>(this.basePath, JSON.stringify(item), this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError))
-  }
-
-  // Get Student by Id
-  getById(id: any): Observable<MySubscription> {
-    return this.http.get<MySubscription>(`${this.basePath}/${id}`, this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError))
-  }
-
-  // Get All Students
-  getAll(): Observable<MySubscription> {
-    return this.http.get<MySubscription>(this.basePath, this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError))
-  }
-
-  // Update Student
-  update(id: any, item: any): Observable<MySubscription> {
-    return this.http.post<MySubscription>(`${this.basePath}/${id}`, JSON.stringify(item), this.httpOptions)
-      .pipe(
-        retry(2),
-        catchError(this.handleError))
-  }
-
-  // Delete Student
-  delete(id: any): Observable<MySubscription> {
-    return this.http.delete<MySubscription>(`${this.basePath}/${id}`, this.httpOptions)
+  getPlans(): Observable<Plan> {
+    return this.http.get<Plan>(this.basePath, this.httpOptions)
       .pipe(
         retry(2),
         catchError(this.handleError))
