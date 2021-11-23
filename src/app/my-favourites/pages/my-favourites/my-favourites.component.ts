a31c848ef63dee69afd79eff8a79a257c6114ec8
SebastianBran <sebastianbranzapata@gmail.com>
2021-11-23 00:11:19 -0500

    feat: refactor my-favourites section

--- src/app/my-favourites/pages/my-favourites/my-favourites.component.ts
+++ src/app/my-favourites/pages/my-favourites/my-favourites.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {MyFavouritesService} from "../../services/my-favourites.service";
-import {ActivatedRoute} from "@angular/router";
 import {CarsService} from "../../../search-car/services/cars.service";
 import {MyFavourites} from "../../model/my-favourites";
 import {Car} from "../../../search-car/model/car";
 import {ClientService} from "../../../my-profile/services/client.service";
+import {CarModelsService} from "../../../search-car/services/car-models.service";
+import {CarBrandsService} from "../../../search-car/services/car-brands.service";
 
 @Component({
   selector: 'app-my-favourites',
@@ -12,16 +13,18 @@ import {ClientService} from "../../../my-profile/services/client.service";
   styleUrls: ['./my-favourites.component.css']
 })
 export class MyFavouritesComponent implements OnInit {
-  clientId!: string | null;
+  clientId!: number;
   favouritesData: MyFavourites[];
   carsData: Car[];
 
   constructor(
     private clientService: ClientService,
     private favouriteService: MyFavouritesService,
-    private carService: CarsService
+    private carService: CarsService,
+    private carModelsService: CarModelsService,
+    private carBrandsService: CarBrandsService,
   ) {
-    this.clientId = localStorage.getItem('clientId');
+    this.clientId = parseInt(<string>localStorage.getItem('clientId'));
     this.favouritesData = [];
     this.carsData = [];
   }
@@ -33,15 +36,34 @@ export class MyFavouritesComponent implements OnInit {
   retrieveCars() {
     this.favouritesData.map(favourite => {
       this.carService.getById(favourite.carId).subscribe((response: any) => {
-        this.carsData.push(response)
+        this.carsData.push(response);
       });
     });
   }
 
+  getModelName(index: number, carModelId: number): any {
+    this.carModelsService.getById(carModelId).subscribe((response: any) => {
+      this.carsData[index].model = response.name;
+      this.getBrandName(index, response.carBrandId);
+    });
+  }
+
+  getBrandName(index: number, carBrandId: number): any {
+    this.carBrandsService.getById(carBrandId).subscribe((response: any) => {
+      this.carsData[index].brand = response.name;
+    });
+  }
+
   retrieveFavourites() {
-    /*this.clientService.getFavoritesByIdClient(this.clientId).subscribe((response: any) => {
-      this.favouritesData = response;
+    this.favouriteService.getByClientId(this.clientId).subscribe((response: any) => {
+      this.favouritesData = response.content;
       this.retrieveCars();
-    });*/
+
+      setTimeout(() => {
+        for (let i = 0; i < this.carsData.length; i++) {
+          this.getModelName(i, this.carsData[i].carModelId);
+        }
+      }, 500);
+    });
   }
 }
